250230595c553b4c972f8880a078167dcf92bc41
hackermdch <58121864+hackermdch@users.noreply.github.com>
2023-10-15 21:40:38 +0800

    Some updates
    Add creative mode tab
    Add some items
    Add localization for tooltip

--- src/main/java/com/primogemstudio/primogemcraft/items/PrimogemCraftItems.java
+++ src/main/java/com/primogemstudio/primogemcraft/items/PrimogemCraftItems.java
@@ -5,15 +5,20 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 
 import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.MOD_ID;
 import static com.primogemstudio.primogemcraft.blocks.PrimogemCraftBlocks.*;
 
 public class PrimogemCraftItems {
+    public static Item PRIMOGEM;
     public static BlockItem DENDRO_CORE_BLOCK_ITEM;
+    public static Item VAYUDA_TURQUOISE_GEMSTONE;
 
     public static void init() {
+        PRIMOGEM = register("primogem", new Item(new Item.Properties().rarity(Rarity.EPIC)));
         DENDRO_CORE_BLOCK_ITEM = register("dendro_core_block", new BlockItem(DENDRO_CORE_BLOCK, new Item.Properties()));
+        VAYUDA_TURQUOISE_GEMSTONE = register("vayuda_turquoise_gemstone", new Item(new Item.Properties()));
     }
 
     private static <T extends Item> T register(String id, T item) {
