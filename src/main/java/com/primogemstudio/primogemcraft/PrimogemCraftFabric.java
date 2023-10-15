45d36b18412e71918f4323eb9aa32416474ac390
hackermdch <58121864+hackermdch@users.noreply.github.com>
2023-10-15 20:07:04 +0800

    Optimized code

--- src/main/java/com/primogemstudio/primogemcraft/PrimogemCraftFabric.java
+++ src/main/java/com/primogemstudio/primogemcraft/PrimogemCraftFabric.java
@@ -2,10 +2,15 @@ package com.primogemstudio.primogemcraft;
 
 import com.primogemstudio.primogemcraft.blocks.PrimogemCraftBlocks;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PrimogemCraftFabric implements ModInitializer {
-    public static String MODID = "primogemcraft";
+    public static String MOD_ID = "primogemcraft";
+    public static Logger logger = LogManager.getLogger();
+
     public void onInitialize() {
         PrimogemCraftBlocks.init();
+        logger.info("Primogem Craft Loaded");
     }
 }
--- src/main/java/com/primogemstudio/primogemcraft/blocks/PrimogemCraftBlocks.java
+++ src/main/java/com/primogemstudio/primogemcraft/blocks/PrimogemCraftBlocks.java
@@ -1,19 +1,21 @@
 package com.primogemstudio.primogemcraft.blocks;
 
-import com.primogemstudio.primogemcraft.blocks.instances.GrassCoreBlock;
+import com.primogemstudio.primogemcraft.blocks.instances.DendroCoreBlock;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
-import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.MODID;
+import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.MOD_ID;
 
 public class PrimogemCraftBlocks {
-    private static GrassCoreBlock GRASS_CORE_BLOCK;
+    public static DendroCoreBlock DENDRO_CORE_BLOCK;
+
     public static void init() {
-        GRASS_CORE_BLOCK = Registry.register(
-                BuiltInRegistries.BLOCK,
-                new ResourceLocation(MODID, "caoyuanhekuai"),
-                new GrassCoreBlock()
-        );
+        DENDRO_CORE_BLOCK = register("dendro_core_block", new DendroCoreBlock());
+    }
+
+    private static <T extends Block> T register(String id, T block) {
+        return Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(MOD_ID, id), block);
     }
 }
