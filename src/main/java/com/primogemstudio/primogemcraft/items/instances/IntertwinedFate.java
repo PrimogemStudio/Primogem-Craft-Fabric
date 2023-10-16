5e32a3bea24ce45e04fae170f9e6d84096ec37d8
hackermdch <58121864+hackermdch@users.noreply.github.com>
2023-10-16 22:38:43 +0800

    Update IntertwinedFate

--- src/main/java/com/primogemstudio/primogemcraft/items/instances/IntertwinedFate.java
+++ src/main/java/com/primogemstudio/primogemcraft/items/instances/IntertwinedFate.java
@@ -1,17 +1,17 @@
 package com.primogemstudio.primogemcraft.items.instances;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
-public class IntertwinedFate extends Item {
+public class IntertwinedFate extends BowItem implements Vanishable {
     public IntertwinedFate() {
         super(new Properties().rarity(Rarity.UNCOMMON));
     }
 
     @Override
+    @NotNull
     public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
         //TODO
         return super.finishUsingItem(stack, level, livingEntity);
