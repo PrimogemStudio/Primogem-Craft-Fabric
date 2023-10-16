0723be9acfc80f26d7b90761c4c0034d8a802732
Jack253-png <guoxiuchen20170402@163.com>
2023-10-16 20:16:59 +0800

    实体添加 (第二阶段)

--- src/main/java/com/primogemstudio/primogemcraft/entities/PrimogemCraftEntities.java
+++ src/main/java/com/primogemstudio/primogemcraft/entities/PrimogemCraftEntities.java
@@ -1,16 +1,27 @@
 package com.primogemstudio.primogemcraft.entities;
 
+import com.primogemstudio.primogemcraft.entities.instances.entities.DendroCoreEntity;
+import com.primogemstudio.primogemcraft.entities.instances.renderer.DendroCoreEntityRenderer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 
 import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.MOD_ID;
 
 public class PrimogemCraftEntities {
+    public static EntityType<DendroCoreEntity> DENDRO_CORE_ENTITY;
     public static void init() {
-
+        DENDRO_CORE_ENTITY = register("dendro_core_entity", EntityType.Builder.of(DendroCoreEntity::new, MobCategory.MONSTER).sized(0.2f, 0.2f));
+        EntityRendererRegistry.register(
+                DENDRO_CORE_ENTITY,
+                DendroCoreEntityRenderer::new
+        );
     }
 
     private static <T extends Entity> EntityType<T> register(String registryname, EntityType.Builder<T> entityTypeBuilder) {
