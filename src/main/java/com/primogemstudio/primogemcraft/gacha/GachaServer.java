8114719456cb3536f535d4db4b123d5603200607
hackermdch <58121864+hackermdch@users.noreply.github.com>
2023-10-18 19:41:13 +0800

    Rollback

--- src/main/java/com/primogemstudio/primogemcraft/gacha/GachaServer.java
+++ src/main/java/com/primogemstudio/primogemcraft/gacha/GachaServer.java
@@ -1,30 +1,22 @@
 package com.primogemstudio.primogemcraft.gacha;
 
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.nbt.CompoundTag;
 
 import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.LOGGER;
 import static com.primogemstudio.primogemcraft.gacha.GachaNetworkConstants.GACHA_TRIGGER;
 
 public class GachaServer {
     public static void init() {
-        ServerPlayNetworking.registerGlobalReceiver(
-                GACHA_TRIGGER,
-                (server, player, handler, buf, responseSender) -> {
-                    System.out.println(buf.readNbt());
-                }
-        );
+        ServerPlayNetworking.registerGlobalReceiver(GACHA_TRIGGER, (server, player, handler, buf, responseSender) -> {
+            LOGGER.warn(buf.readNbt());
+        });
     }
 
-    public static CompoundTag loadData(CompoundTag data) {
+    public static void loadData() {
         LOGGER.info("Data reading!");
-        var test = data.getInt("TestData");
-        LOGGER.info("TestData is: " + test);
-        return data;
     }
 
-    public static void saveData(CompoundTag data) {
-        data.putInt("TestData", 32768);
+    public static void saveData() {
         LOGGER.info("Data saving!");
     }
 }
--- src/main/java/com/primogemstudio/primogemcraft/mixin/LevelStorageSourceMixin.java
+++ src/main/java/com/primogemstudio/primogemcraft/mixin/LevelStorageSourceMixin.java
@@ -1,28 +1,45 @@
 package com.primogemstudio.primogemcraft.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.Lifecycle;
+import com.primogemstudio.primogemcraft.PrimogemCraftFabric;
 import com.primogemstudio.primogemcraft.gacha.GachaServer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.world.level.WorldDataConfiguration;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.WorldDimensions;
 import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.WorldData;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LevelStorageSource.class)
 public class LevelStorageSourceMixin {
-    @WrapOperation(method = "method_29582", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getCompound(Ljava/lang/String;)Lnet/minecraft/nbt/CompoundTag;", ordinal = 0))
-    private static CompoundTag onGetDataTag(CompoundTag instance, String key, Operation<CompoundTag> original) {
-        return GachaServer.loadData(original.call(instance, key));
-    }
-
     @Mixin(LevelStorageSource.LevelStorageAccess.class)
     public static class LevelStorageAccessMixin {
-        @ModifyArg(method = "saveDataTag(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/level/storage/WorldData;Lnet/minecraft/nbt/CompoundTag;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;put(Ljava/lang/String;Lnet/minecraft/nbt/Tag;)Lnet/minecraft/nbt/Tag;"), index = 1)
-        public Tag onSaveDataTag(Tag data) {
-            GachaServer.saveData((CompoundTag) data);
-            return data;
+        @Shadow
+        @Final
+        LevelStorageSource.LevelDirectory levelDirectory;
+
+        @Inject(at = @At("HEAD"), method = "getDataTag")
+        public void onGetDataTag(DynamicOps<Tag> ops, WorldDataConfiguration dataConfiguration, Registry<LevelStem> levelStemRegistry, Lifecycle lifecycle, CallbackInfoReturnable<@Nullable Pair<WorldData, WorldDimensions.Complete>> cir) {
+            GachaServer.loadData();
+        }
+
+        @Inject(at = @At("HEAD"), method = "saveDataTag(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/level/storage/WorldData;Lnet/minecraft/nbt/CompoundTag;)V")
+        public void onSaveDataTag(RegistryAccess registries, WorldData serverConfiguration, CompoundTag hostPlayerNBT, CallbackInfo ci) {
+            PrimogemCraftFabric.LOGGER.error(levelDirectory.path().toString());
+            GachaServer.saveData();
         }
     }
 }
