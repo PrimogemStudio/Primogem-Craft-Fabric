d754a8432608a35ab068fb434a9255c15f0ec355
Jack253-png <guoxiuchen20170402@163.com>
2023-10-19 18:02:26 +0800

    Update DendroPickaxe.java

--- src/main/java/com/primogemstudio/primogemcraft/items/instances/DendroPickaxe.java
+++ src/main/java/com/primogemstudio/primogemcraft/items/instances/DendroPickaxe.java
@@ -2,9 +2,41 @@ package com.primogemstudio.primogemcraft.items.instances;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
 
 public class DendroPickaxe extends PickaxeItem {
     public DendroPickaxe() {
-        super(null, 1, -3f, new Item.Properties().fireResistant());
+        super(new Tier() {
+            @Override
+            public int getUses() {
+                return 0;
+            }
+
+            @Override
+            public float getSpeed() {
+                return 0;
+            }
+
+            @Override
+            public float getAttackDamageBonus() {
+                return 0;
+            }
+
+            @Override
+            public int getLevel() {
+                return 0;
+            }
+
+            @Override
+            public int getEnchantmentValue() {
+                return 0;
+            }
+
+            @Override
+            public Ingredient getRepairIngredient() {
+                return null;
+            }
+        }, 1, -3f, new Item.Properties().fireResistant());
     }
 }
