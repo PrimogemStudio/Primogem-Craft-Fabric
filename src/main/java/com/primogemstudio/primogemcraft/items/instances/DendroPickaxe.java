f980320e05cc802f962558c108dee2b9cd302758
Jack253-png <guoxiuchen20170402@163.com>
2023-10-19 17:58:50 +0800

    Update build.gradle

---
+++ src/main/java/com/primogemstudio/primogemcraft/items/instances/DendroPickaxe.java
@@ -0,0 +1,10 @@
+package com.primogemstudio.primogemcraft.items.instances;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+
+public class DendroPickaxe extends PickaxeItem {
+    public DendroPickaxe() {
+        super(null, 1, -3f, new Item.Properties().fireResistant());
+    }
+}
