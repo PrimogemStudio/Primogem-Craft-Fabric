f912e0b4a1b7be57e78a7e8665c7194929593dcf
Jack253-png <guoxiuchen20170402@163.com>
2023-10-19 19:42:05 +0800

    回滚抽卡核心服务器端

--- src/main/java/com/primogemstudio/primogemcraft/gacha/GachaServer.java
+++ src/main/java/com/primogemstudio/primogemcraft/gacha/GachaServer.java
@@ -3,6 +3,7 @@ package com.primogemstudio.primogemcraft.gacha;
 import com.google.gson.Gson;
 import com.primogemstudio.primogemcraft.gacha.serialize.TestData;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.nbt.NbtIo;
 
 import java.io.*;
 import java.nio.file.Path;
@@ -20,32 +21,11 @@ public class GachaServer {
     }
 
     public static void loadData() {
-        var file = currentDir.resolve("1.json").toFile();
-        if (file.exists()) {
-            var gson = new Gson();
-            try (var fr = new FileReader(file)) {
-                var data = gson.fromJson(fr, TestData.class);
-                LOGGER.info("Data read!");
-                LOGGER.error(data);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        LOGGER.info("Data reading!");
     }
 
     public static void saveData() {
-        var file = currentDir.resolve("1.json").toFile();
-        var gson = new Gson();
-        try (var fo = new FileOutputStream(file)) {
-            var data = new TestData("abc", 10);
-            data.inner = new TestData.Inner();
-            data.inner.num = 0.1231234f;
-            data.inner.array = new int[]{5, 66, 7, 8, 9, 1, 2, 3};
-            fo.write(gson.toJson(data).getBytes());
-            LOGGER.info("Data saved!");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        LOGGER.info("Data saving!");
     }
 
     public static void onDataChange() {
---
+++ src/main/java/com/primogemstudio/primogemcraft/gacha/serialize/GachaRecordModel.java
@@ -0,0 +1,12 @@
+package com.primogemstudio.primogemcraft.gacha.serialize;
+
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.UUID;
+
+public class GachaRecordModel {
+    public String name;
+    public UUID uuid;
+    public int level;
+    public ResourceLocation item;
+}
