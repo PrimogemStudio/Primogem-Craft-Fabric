1779122ce542a35fc62507abfa46fa4ac5d61051
Jack253-png <guoxiuchen20170402@163.com>
2023-10-21 17:22:18 +0800

    数据库改用 SQLite

--- src/main/java/com/primogemstudio/primogemcraft/database/GachaDatabase.java
+++ src/main/java/com/primogemstudio/primogemcraft/database/GachaDatabase.java
@@ -1,7 +1,13 @@
 package com.primogemstudio.primogemcraft.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class GachaDatabase {
-    public static void init() throws Exception {
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/test_gacha_data.db");
+        conn.createStatement().executeUpdate("create table if not exists gacha_pity(id integer primary key autoincrement,username varchar(64),uuid varchar(32),pity5 integer, pity4 integer)");
 
     }
 }
