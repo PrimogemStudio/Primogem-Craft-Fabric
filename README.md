e35a5fd8aa66d6aa67246e823da2af0b02dbdee6
Jack253-png <guoxiuchen20170402@163.com>
2023-10-22 17:53:43 +0800

    更新 README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Primogem-Craft-Fabric
 Primogem craft port for fabric
+
+### Current status: 
+    - Gacha System 
+        -- Database / Backend management (finished)
+        -- In game experience (planned)
+    - Weapons & Tools (Working)
--- src/main/java/com/primogemstudio/primogemcraft/CreativeTab.java
+++ src/main/java/com/primogemstudio/primogemcraft/CreativeTab.java
@@ -14,7 +14,8 @@ import static com.primogemstudio.primogemcraft.items.PrimogemCraftItems.*;
 
 public class CreativeTab {
     public static final ResourceKey<CreativeModeTab> KEY = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MOD_ID, "primogemcraft_tab"));
-
+    public static final ResourceKey<CreativeModeTab> KEY_BLOCKS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MOD_ID, "primogemcraft_blocks_tab"));
+    public static final ResourceKey<CreativeModeTab> KEY_TOOLS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MOD_ID, "primogemcraft_tools_tab"));
     public static void init() {
         Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, KEY, CreativeModeTab.builder(CreativeModeTab.Row.TOP, 1).title(Component.translatable("itemGroup.primogemcraft")).icon(() -> new ItemStack(PRIMOGEM)).displayItems((itemDisplayParameters, output) -> {
             output.accept(PRIMOGEM);
@@ -22,11 +23,11 @@ public class CreativeTab {
             output.accept(VAYUDA_TURQUOISE_GEMSTONE);
         }).build());
 
-        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, KEY, CreativeModeTab.builder(CreativeModeTab.Row.TOP, 1).title(Component.translatable("itemGroup.primogemcraft_blocks")).icon(() -> new ItemStack(PRIMOGEM)).displayItems((itemDisplayParameters, output) -> {
+        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, KEY_BLOCKS, CreativeModeTab.builder(CreativeModeTab.Row.TOP, 1).title(Component.translatable("itemGroup.primogemcraft_blocks")).icon(() -> new ItemStack(PRIMOGEM)).displayItems((itemDisplayParameters, output) -> {
             output.accept(DENDRO_CORE_BLOCK_ITEM);
         }).build());
 
-        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, KEY, CreativeModeTab.builder(CreativeModeTab.Row.TOP, 1).title(Component.translatable("itemGroup.primogemcraft_weapon_tools")).icon(() -> new ItemStack(PRIMOGEM)).displayItems((itemDisplayParameters, output) -> {
+        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, KEY_TOOLS, CreativeModeTab.builder(CreativeModeTab.Row.TOP, 1).title(Component.translatable("itemGroup.primogemcraft_weapon_tools")).icon(() -> new ItemStack(PRIMOGEM)).displayItems((itemDisplayParameters, output) -> {
             output.accept(DENDRO_PICKAXE);
         }).build());
     }
