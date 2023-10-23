7eafe8319f73bd1c408801cdf3084656dc15db2b
Jack253-png <guoxiuchen20170402@163.com>
2023-10-23 11:10:26 +0800

    Fix sounds

--- src/main/java/com/primogemstudio/primogemcraft/sounds/PrimogemCraftSounds.java
+++ src/main/java/com/primogemstudio/primogemcraft/sounds/PrimogemCraftSounds.java
@@ -1,5 +1,7 @@
 package com.primogemstudio.primogemcraft.sounds;
 
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
@@ -8,6 +10,10 @@ import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.MOD_ID;
 public class PrimogemCraftSounds {
     public static SoundEvent PRE_GACHA;
     public static void init() {
-        PRE_GACHA = SoundEvent.createVariableRangeEvent(new ResourceLocation(MOD_ID, "pre_gacha"));
+        PRE_GACHA = register("pre_gacha", SoundEvent.createVariableRangeEvent(new ResourceLocation(MOD_ID, "pre_gacha")));
+
+    }
+    public static <T extends SoundEvent> T register(String name, T event) {
+        return Registry.register(BuiltInRegistries.SOUND_EVENT, new ResourceLocation(MOD_ID, name), event);
     }
 }
