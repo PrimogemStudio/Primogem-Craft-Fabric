b1e3a6d5f6419ce0cd9adc8ba59cf931ce54b7f2
Jack253-png <guoxiuchen20170402@163.com>
2023-10-24 20:34:15 +0800

    add abstraction layer (Database interface)

--- src/main/java/com/primogemstudio/primogemcraft/gacha/GachaNetworkConstants.java
+++
@@ -1,9 +0,0 @@
-package com.primogemstudio.primogemcraft.gacha;
-
-import net.minecraft.resources.ResourceLocation;
-
-import static com.primogemstudio.primogemcraft.PrimogemCraftFabric.MOD_ID;
-
-public class GachaNetworkConstants {
-    public static ResourceLocation GACHA_TRIGGER = new ResourceLocation(MOD_ID, "gacha_trigger");
-}
