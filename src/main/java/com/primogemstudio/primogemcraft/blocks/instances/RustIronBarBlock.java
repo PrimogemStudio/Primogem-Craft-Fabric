c757fa3e0f59088e56c788d36394f629559bc423
hackermdch <58121864+hackermdch@users.noreply.github.com>
2023-12-01 21:06:53 +0800

    Add rust iron bar

---
+++ src/main/java/com/primogemstudio/primogemcraft/blocks/instances/RustIronBarBlock.java
@@ -0,0 +1,9 @@
+package com.primogemstudio.primogemcraft.blocks.instances;
+
+import net.minecraft.world.level.block.Block;
+
+public class RustIronBarBlock extends Block {
+    public RustIronBarBlock() {
+        super(Properties.of());
+    }
+}
