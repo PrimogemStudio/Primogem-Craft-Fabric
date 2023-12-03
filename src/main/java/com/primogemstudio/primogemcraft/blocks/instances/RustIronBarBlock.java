01bbd6b969b959b1a01c37f782962ed4aa2e8a19
Jack253-png <guoxiuchen20170402@163.com>
2023-12-03 11:17:32 +0800

    修复 "锈铁栅栏" 模型显示问题

--- src/main/java/com/primogemstudio/primogemcraft/blocks/instances/RustIronBarBlock.java
+++ src/main/java/com/primogemstudio/primogemcraft/blocks/instances/RustIronBarBlock.java
@@ -1,8 +1,8 @@
 package com.primogemstudio.primogemcraft.blocks.instances;
 
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.IronBarsBlock;
 
-public class RustIronBarBlock extends Block {
+public class RustIronBarBlock extends IronBarsBlock {
     public RustIronBarBlock() {
         super(Properties.of());
     }
